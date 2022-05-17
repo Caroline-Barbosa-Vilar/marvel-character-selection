76360ec9369328aa07240bbcdca3ff857308ed98
Caroline Barbosa <bo.varig@gmail.com>
2022-05-17 20:09:20 -0300

    condition to change the character name

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,11 @@ const characters = document.querySelectorAll('.character__item')
 
 characters.forEach((character__item) => {
   character__item.addEventListener('mouseenter', () => {
+
+    const selectedId = character__item.attributes.id.value;
+
+    if (selectedId === 'ultron') return;
+    
     const selectedCharacter = document.querySelector('.selected')
     selectedCharacter.classList.remove('selected')
 
