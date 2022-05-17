e364b3534a4e2a0e2571d20c051bd7980adc4abb
Caroline Barbosa <bo.varig@gmail.com>
2022-05-17 17:28:05 -0300

    created: the action that remove the tag from the item that is not selected

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,9 @@ const characters = document.querySelectorAll('.character__item')
 
 characters.forEach((character__item) => {
   character__item.addEventListener('mouseenter', () => {
+    const selectedCharacter = document.querySelector('.selected')
+    selectedCharacter.classList.remove('selected')
+
     character__item.classList.add('selected')
   })
 }) 
