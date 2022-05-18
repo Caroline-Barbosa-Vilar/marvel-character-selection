8d415cbeff6c85b73702709041aa317393a563f9
Caroline Barbosa <bo.varig@gmail.com>
2022-05-17 21:11:30 -0300

    fixed the bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const characters = document.querySelectorAll('.character__item')
+const characters = document.querySelectorAll('.character__item');
 
 characters.forEach((character__item) => {
   character__item.addEventListener('mouseenter', () => {
@@ -6,10 +6,18 @@ characters.forEach((character__item) => {
     const selectedId = character__item.attributes.id.value;
 
     if (selectedId === 'ultron') return;
-    
-    const selectedCharacter = document.querySelector('.selected')
-    selectedCharacter.classList.remove('selected')
 
-    character__item.classList.add('selected')
+    const selectedCharacter = document.querySelector('.selected');
+    selectedCharacter.classList.remove('selected');
+
+    character__item.classList.add('selected');
+
+    const player1Image = document.getElementById('character-player-1');
+    player1Image.src = `./src/img/${selectedId}.png`;
+
+    const player1Name = document.getElementById('character__name-1');
+    const selectedName = character__item.getAttribute('data-name');
+
+    player1Name.innerHTML = selectedName;
   })
 }) 
